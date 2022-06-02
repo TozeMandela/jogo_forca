df328b12a1d3a9cbdb9d1f8b280cb3ba1c050f70
Tozé Mandela <mandelajunior10@gmail.com>
2022-06-02 23:15:58 +0100

    a

--- src/Controller/forcaController.js
+++ src/Controller/forcaController.js
@@ -1,18 +1,263 @@
 let sorteios= [{ sugestao: 'fruta',
-                Element : ['banana','ananas',
-                            'manga','figo',
-                            'laranja','pera',
-                            'maçã','maracujã',
-                            'mamau','melância',
-                            'goiaba','abacate'
+                Element : [
+                    "Abacate"
+                    ,"Abacaxi"
+                    ,"Abiu"
+                    ,"Abricó"
+                    ,"Abrunho"
+                    ,"Açaí"
+                    ,"Acerola"
+                    ,"Akee"
+                    ,"Alfarroba"
+                    ,"Ameixa"
+                    ,"Amêndoa"
+                    ,"Amora"
+                    ,"Ananás"
+                    ,"Anona"
+                    ,"Araçá"
+                    ,"Arando"
+                    ,"Araticum"
+                    ,"Ata"
+                    ,"Atemoia"
+                    ,"Avelã"
+                    ,"Babaco"
+                    ,"Babaçu"
+                    ,"Bacaba"
+                    ,"Bacuri"
+                    ,"Bacupari"
+                    ,"Banana"
+                    ,"Baru"
+                    ,"Bergamota"
+                    ,"Biribá"
+                    ,"Buriti"
+                    ,"Butiá"
+                    ,"Cabeludinha"
+                    ,"Cacau"
+                    ,"Cagaita"
+                    ,"Caimito"
+                    ,"Cajá"
+                    ,"Caju"
+                    ,"Calabaça"
+                    ,"Calabura"
+                    ,"Calamondin"
+                    ,"Cambucá"
+                    ,"Cambuci"
+                    ,"Camu-camu"
+                    ,"Caqui"
+                    ,"Carambola"
+                    ,"Carnaúba"
+                    ,"Castanha"
+                    ,"Castanha-do-pará"
+                    ,"Cereja"
+                    ,"Ciriguela"
+                    ,"Ciruela"
+                    ,"Coco"
+                    ,"Cranberry"
+                    ,"Cupuaçu"
+                    ,"Damasco"
+                    ,"Dekopon"
+                    ,"Dendê"
+                    ,"Dióspiro"
+                    ,"Dovyalis"
+                    ,"Durião"
+                    ,"Embaúba"
+                    ,"Embaubarana"
+                    ,"Engkala"
+                    ,"Escropari"
+                    ,"Esfregadinha"
+                    ,"Figo"
+                    ,"Framboesa"
+                    ,"Fruta do conde"
+                    ,"Fruta pão"
+                    ,"Feijoa"
+                    ,"Figo da índia"
+                    ,"Fruta de cedro"
+                    ,"Fruta de lobo"
+                    ,"Fruta do milagre"
+                    ,"Fruta de tatu"
+                    ,"Gabiroba"
+                    ,"Glicosmis"
+                    ,"Goiaba"
+                    ,"Granadilla"
+                    ,"Gravatá"
+                    ,"Graviola"
+                    ,"Groselha"
+                    ,"Grumixama"
+                    ,"Guabiju"
+                    ,"Guabiroba"
+                    ,"Guaraná"
+                    ,"Hawthorn"
+                    ,"Heisteria"
+                    ,"Hilocéreo"
+                    ,"Ibacurupari"
+                    ,"Ilama"
+                    ,"Imbe"
+                    ,"Imbu"
+                    ,"Inajá"
+                    ,"Ingá"
+                    ,"Inharé"
+                    ,"Jabuticaba"
+                    ,"Jaca"
+                    ,"Jambo"
+                    ,"Jambolão"
+                    ,"Jamelão"
+                    ,"Jaracatiá"
+                    ,"Jatobá"
+                    ,"Jenipapo"
+                    ,"Jerivá"
+                    ,"Juá"
+                    ,"Jujuba"
+                    ,"Kiwi"
+                    ,"Kumquat"
+                    ,"Kinkan"
+                    ,"Kino"
+                    ,"Kiwano"
+                    ,"Kabosu"
+                    ,"Karité"
+                    ,"Laranja"
+                    ,"Limão"
+                    ,"Lima"
+                    ,"Lichia"
+                    ,"Longan"
+                    ,"Lucuma"
+                    ,"Lacucha"
+                    ,"Lulo"
+                    ,"Lobeira"
+                    ,"Langsat"
+                    ,"Laranja-de-pacu"
+                    ,"Mabolo"
+                    ,"Maçã"
+                    ,"Macadâmia"
+                    ,"Macaúba"
+                    ,"Mamão"
+                    ,"Mamey"
+                    ,"Mamoncillo"
+                    ,"Maná-cubiu"
+                    ,"Manga"
+                    ,"Mangaba"
+                    ,"Mangostão"
+                    ,"Maracujá"
+                    ,"Marang"
+                    ,"Marmelo"
+                    ,"Marolo"
+                    ,"Marula"
+                    ,"Massala"
+                    ,"Melancia"
+                    ,"Melão"
+                    ,"Meloa"
+                    ,"Mexerica"
+                    ,"Mirtilo"
+                    ,"Morango"
+                    ,"Murici"
+                    ,"Naranjilla"
+                    ,"Nectarina"
+                    ,"Nêspera"
+                    ,"Noni"
+                    ,"Noz"
+                    ,"Noz-pecã"
+                    ,"Noz-macadâmia"
+                    ,"Oiti"
+                    ,"Oxicoco"
+                    ,"Orangelo"
+                    ,"Pera"
+                    ,"Pêssego"
+                    ,"Pitanga"
+                    ,"Pinha"
+                    ,"Pitaia"
+                    ,"Pitomba"
+                    ,"Pitangatuba"
+                    ,"Pindaíba"
+                    ,"Pequi"
+                    ,"Pequiá"
+                    ,"Physalis"
+                    ,"Pulasan"
+                    ,"Pomelo"
+                    ,"Pupunha"
+                    ,"Puçá"
+                    ,"Patauá"
+                    ,"Pajurá"
+                    ,"Pixirica"
+                    ,"Pistache"
+                    ,"Quina"
+                    ,"Quiuí"
+                    ,"Romã"
+                    ,"Rambai"
+                    ,"Rambutão"
+                    ,"Rukam"
+                    ,"Saguaraji"
+                    ,"Salak"
+                    ,"Santol"
+                    ,"Sapota"
+                    ,"Sapoti"
+                    ,"Sapucaia"
+                    ,"Saputá"
+                    ,"Seriguela"
+                    ,"Sorvinha"
+                    ,"Tangerina"
+                    ,"Tamarindo"
+                    ,"Tâmara"
+                    ,"Toranja"
+                    ,"Tucumã"
+                    ,"Taiuva"
+                    ,"Tapiá"
+                    ,"Tarumã"
+                    ,"Tangor"
+                    ,"Tucujá"
+                    ,"Uva"
+                    ,"Umbu"
+                    ,"Uvaia"
+                    ,"Uchuva"
+                    ,"Umê"
+                    ,"Uxi"
+                    ,"Vacínio"
+                    ,"Veludo"
+                    ,"Vergamota"
+                    ,"Wampi"
+                    ,"Xixá"
+                    ,"Yamamomo"
+                    ,"Yuzu"
+                    ,"Zimbro"
                         ]
             },{ sugestao: 'cantores do Morrão',
                  Element : ['Alpha O Pica','durões',
                         'bufalo','new class 00',
-                        'negão','pera',
+                        'negão','peraBem',
                         'dj naype','miro star boy'
                     ]
-        }];
+        },{ sugestao: 'nome de Carros',
+        Element : ["Agrale"
+                    ,"Audi"
+                    ,"bmw"
+                    ,"Bianco"
+                    ,"Brasinca"
+                    ,"Chamonix"
+                    ,"Chevrolet"
+                    ,"Citroën"
+                    ,"DKW-Vemag"
+                    ,"Dodge"
+                    ,"Engesa"
+                    ,"Envemo"
+                    ,"Fiat"
+                    ,"FNM"
+                    ,"Ford"
+                    ,"Gurgel"
+                    ,"Honda"
+                    ,"Indústrias Romi"
+                    ,"Lafer S/A"
+                    ,"Lobini"
+                    ,"Mahindra by Bramont"
+                    ,"Mercedes-Benz"
+                    ,"Miúra"
+                    ,"Peugeot – PSA Peugeot Citroën"
+                    ,"Puma"
+                    ,"Renault"
+                    ,"Romi"
+                    ,"Simca do Brasil"
+                    ,"Toyota"
+                    ,"Troller"
+                    ,"Volkswagen"
+                    ,"Willys Overland"]
+}];
 let $mainAside = document.querySelector('main>.left');
 let $mainSection = document.querySelector('main>.rigth');
 let $suggestion = document.querySelector('section>.sugestao>h2>.spansuggest');
@@ -23,8 +268,8 @@ let $vida = document.querySelector('aside>div.vida>h2>span');
 let button = document.querySelectorAll('section>aside>nav>div');
 let $img = document.querySelector('main>aside>img.perdedor');
 let indexImg = 6;
-
-console.log($mainAside,'\n',$mainSection)
+let Ctjogo = false;
+let controlVida = 0;
 function selectIndexElement(arrayPosi){
     return Math.floor(Math.random()*arrayPosi.length-1)+1;
 }
@@ -53,14 +298,22 @@ function initializeVariables(){
     indexElemenSelected = selectIndexElement(sorteios[indexSuggestSelected].Element);
     elementSelected = remuveElementSelectedToArray(sorteios[indexSuggestSelected].Element, 
     indexElemenSelected)[0].split('');
+    elementSelected = getTOUpperCase(elementSelected);
     Arraytraco = getTraco(elementSelected);
     chance = chances(Arraytraco);
 
     $suggestion.innerHTML=sorteios[indexSuggestSelected].sugestao;
-    console.log(elementSelected, indexElemenSelected, sorteios[indexSuggestSelected].Element)
+    console.log(elementSelected, indexElemenSelected, sorteios[indexSuggestSelected].Element, Arraytraco)
 }
 
 initializeVariables();
+
+function getTOUpperCase(array){  
+    for(let i =0; i < array.length; i++){ 
+        array[i] = array[i].toUpperCase();
+    }
+    return array;
+}
 
 function substValue(str, value){
     let arrAux = [];
@@ -83,13 +336,26 @@ function isWin(arrT, arr2){
         return element!=='_';
     });
     if(aux){
-
+        let idInterval;
         if (isEquals(arrT,arr2)) {
+            selectedImg(0);
             let aux = Number($pontuacao.innerHTML);
-            alert('acertouuu');
             $pontuacao.innerHTML = aux+1;
-            initializeVariables();
+            Ctjogo = true;
+            
         }
+        if (Ctjogo) {
+            initJogo(Ctjogo)
+        }
+        
+    }
+}
+
+function selectedImg(indice){
+    if((indice===1 && indice!==0)|| (indice < 7 && indice!==0)){
+        $img.src=`../../mult-media/img/perdendo/perdendo${indice}.png`;
+    }else if(indice===0){
+        $img.src=`../../mult-media/img/ganhando/ganhando${indice}.png`;
     }
 }
 
@@ -136,7 +402,10 @@ function substractcheArrayTraco(arr, arrTraco, value){
         }else{ 
             chance = chance - 1;
             $vida.innerHTML = aux-10;
-            indexImg = aux%2===0 ?indexImg-=1: indexImg;
+            controlVida = controlVida + 1;
+            if(controlVida % 2 ===0){
+                indexImg-=1;
+            }
         }
     }else{
         $vida.innerHTML = '00';
@@ -145,7 +414,7 @@ function substractcheArrayTraco(arr, arrTraco, value){
         elementSelected =[];
     }
 
-    $img.src=`../../mult-media/img/perdendo/perdendo${indexImg}.png`;
+    selectedImg(indexImg);
 }
 function changeBackGround(vida){
     let aux = Number(vida.innerHTML);
@@ -160,16 +429,28 @@ function changeBackGround(vida){
 button.forEach(btn=>{
     btn.addEventListener('click', function (){
 
-        let indexToBeReplaced = substValue(elementSelected, this.innerHTML);
-        substractcheArrayTraco(indexToBeReplaced,Arraytraco,this.innerHTML);
-        $chance.innerHTML = chance;
+        let Value = this.innerHTML.toUpperCase();
+        let indexToBeReplaced = substValue(elementSelected, Value);
+        substractcheArrayTraco(indexToBeReplaced,Arraytraco,Value);
+        viewData();
         $question.innerHTML= Arraytraco.join(' ');
+        changeBackGround($vida);
         isWin(elementSelected, Arraytraco);
-        $question.innerHTML= Arraytraco.join(' ');
-        changeBackGround($vida)
+
       });
       
 });
-$chance.innerHTML = chance;
-$question.innerHTML= Arraytraco.join(' ');
-
+function initJogo(boll){
+    console.log('entrei...')
+    indexImg = 6;
+    indexImg =6;
+    selectedImg(indexImg)
+    initializeVariables();
+    viewData();
+    boll=false;
+}
+function viewData(){
+    $chance.innerHTML = chance;
+    $question.innerHTML= Arraytraco.join(' ');
+}
+viewData();
